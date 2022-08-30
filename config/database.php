3a3a683b43ec5103f639e0cd816d1bb1dcb5c48c
kurt john <kurtjohn2112>
2022-08-30 20:30:26 +0800

    update

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-east-06.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'heroku_0b31e755b74c07c'),
             'username' => env('DB_USERNAME', 'bb9626b85da02b'),
