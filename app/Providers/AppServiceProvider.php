441efcb9e3aaadba20c0c6b00b7726955b7967d5
kurt john <kurtjohn2112>
2022-08-30 20:57:38 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,5 +29,7 @@ class AppServiceProvider extends ServiceProvider
 
         Paginator::useBootstrap();
         // Paginator::useTailwind();
+
+        Schema::defaultStringLength(191);
     }
 }
