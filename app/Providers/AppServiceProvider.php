394558ff57f9372cc7e80e312cc9ae39b8a40637
kurt john <kurtjohn2112>
2022-08-30 21:18:50 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -30,6 +31,10 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
         // Paginator::useTailwind();
 
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(191); // force schema to have longer string
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
